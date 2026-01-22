3b359890df9bfec50bbf53634bdfa5cadf631c84
unknown <tubeben198@gmail.com>
2026-01-22 12:49:39 +0200

    Merged all the deck_gen functions into one. Also added the possibility of changing the value and probablity properties of the card

--- Deck_gen.R
+++ Deck_gen.R
@@ -20,14 +20,30 @@ check_price = function(deck_type){
 }
 
 # ---------------------------------------------
+# This function will be used to generate the packs. It takes wanted type of deck
+# and all the different value ranges and probability chances may be used to simulate 
+# different situations
 
-gen_normal_deck = function(){
-  # Simulating the cards
-  Card_values = sample(seq(0.1,2,0.01),10, replace = T)
-  C.type.chance = sample(c('Normal','Golden','Holo'), 10, 
-                         prob = c(.98,.012,.008), replace = T)
-  # Assembling the deck
-  deck = data.frame(Value = Card_values, Type = C.type.chance) %>%
+gen_deck = function(Type, Val_norm = Val_norm, Prob_norm = Prob_norm,
+                    Val_unc = Val_unc, Prob_unc = Prob_unc,
+                    Val_spec = Val_spec, Prob_spec = Prob_spec){
+  
+  Card_types = c('Normal','Golden','Holo')
+  
+  if (Type == 'normal'){
+    Card_values = sample(Val_norm, 10, replace = T)
+    C_type_chance = sample(Card_types, 10, prob = Prob_norm, replace = T)
+  }
+  if (Type == 'uncommon'){
+    Card_values = sample(Val_unc, 10, replace = T)
+    C_type_chance = sample(Card_types, 10, prob = Prob_unc, replace = T)
+  }
+  if (Type == 'special'){
+    Card_values = sample(Val_spec, 5, replace = T)
+    C_type_chance = sample(Card_types, 5, prob = Prob_spec, replace = T)
+  }
+  
+  deck = data.frame(Value = Card_values, Type = C_type_chance) %>%
     mutate(Value = case_when(
       Type == 'Golden' ~ Value * 1.5,
       Type == 'Holo' ~ Value *3,
@@ -37,38 +53,6 @@ gen_normal_deck = function(){
 }
 
 # --------------------------------------------
-
-gen_uncommon_deck = function(){
-  Card_values = sample(seq(1,3.5,0.01),10, replace = T)
-  C.type.chance = sample(c('Normal','Golden','Holo'), 10, 
-                         prob = c(.965,.023,.012), replace = T)
-  
-  deck = data.frame(Value = Card_values, Type = C.type.chance) %>%
-    mutate(Value = case_when(
-      Type == 'Golden' ~ Value * 1.5,
-      Type == 'Holo' ~ Value *3,
-      .default = Value))
-  
-  return(deck)
-}
-
-# --------------------------------------------
-
-gen_special_deck = function(){
-  Card_values = sample(seq(7,10,0.01), 5, replace = T)
-  C.type.chance = sample(c('Normal','Golden','Holo'), 5, 
-                         prob = c(.8,.155,.045), replace = T)
-  
-  deck = data.frame(Value = Card_values, Type = C.type.chance) %>%
-    mutate(Value = case_when(
-      Type == 'Golden' ~ Value * 1.5,
-      Type == 'Holo' ~ Value *3,
-      .default = Value))
-  
-  return(deck)
-}
-
-# -------------------------------------------
 
 calc_cost = function(type, balance){
   price = check_price(type)
@@ -76,50 +60,48 @@ calc_cost = function(type, balance){
   return(max_amount)
 }
 
-# -------------------------------------------
 
-buy_deck = function(deck_type, balance){
-  # Watch out for typos
+# ------------------------------------------
+# This will be the function which allows us to buy the decks. It takes the same inputs
+# as the one which generates them, with the addition of balance. By comparing the price to our
+# balance we can check if we can afford the respective deck or not. If we can't buy it, it returns
+# NULL - this part is important
+
+
+buy_deck = function(deck_type, balance,
+                    Val_norm = Val_norm, Prob_norm = Prob_norm,
+                    Val_unc = Val_unc, Prob_unc = Prob_unc,
+                    Val_spec = Val_spec, Prob_spec = Prob_spec){
+  # Check for  typeos
   if (deck_type %notin% c('normal','uncommon','special'))
     stop('Type of deck does not exist, you most likely made a typo')
   
   price = check_price(deck_type)
   
-  if (deck_type == 'special'){
-    if (balance < price){
-      deck_type = 'uncommon'
-      price = check_price(deck_type)
-      message('Not enough money for special deck, buying an uncommon deck instead') 
-    }
-    else {
-      deck = gen_special_deck()
-      money_left = balance-price
-      return(list(deck = deck, type = deck_type, balance = money_left))
-    }
-  }
-
-  if (deck_type == 'uncommon'){
-    if (balance < price){
+  if (balance < price){
+    message("You dont have enough money, let's try an uncommon deck instead")
+    deck_type = 'uncommon'
+    price = check_price(deck_type)
+    
+    if(balance < price){
+      message('You still dont have enough money, probably gonna be enough for a normal deck')
       deck_type = 'normal'
       price = check_price(deck_type)
-      message('Not enough money for uncommon deck, buying a normal deck') 
     }
-    else {
-      deck = gen_uncommon_deck()
-      money_left = balance-price
-      return(list(deck = deck, type = deck_type, balance = money_left))
-    }
+      if (balance < price){ # If we don't the money needed, we get NULL
+        message("You're too poor to buy even a normal deck")
+        return(NULL)
+      }
   }
-
-  if (deck_type == 'normal' & balance >= price){
-    deck = gen_normal_deck()
-    money_left = balance-price
-    return(list(deck = deck, type = deck_type, balance = money_left))
-    }
-   else{
-     message('Not enough money for normal deck either')
-     return(NULL)}
+  
+  money_left = balance - price # calculating our money left after the transaction
+  deck = gen_deck(deck_type,Val_norm = Val_norm, Prob_norm = Prob_norm,
+                            Val_unc = Val_unc, Prob_unc = Prob_unc,
+                            Val_spec = Val_spec, Prob_spec = Prob_spec)
+  
+  # It return a list with all the variables needed, to assemble our result data frame 
+  # in the simulation
+  
+  return(list(deck = deck, type = deck_type, balance = money_left))
 }
-
-
 
--- Simulation.R
+++ Simulation.R
@@ -1,7 +1,12 @@
 library(flexplot)
 source('Deck_gen.R')
 
-sim = function(n, balance){
+sim = function(n, Balance, Prob_deck = c(0.3,0.3,0.3),
+               Val_norm = c(seq(0.1,2,0.01)), Prob_norm = c(.98,.012,.008),
+               Val_unc = seq(1,3.5,0.01), Prob_unc = c(.965,.023,.012), 
+               Val_spec = seq(7,10,0.01), Prob_spec = c(.965,.023,.012)){
+  
+    start = Sys.time()
   # Initializing a data frame which will contain the parameters of the opened decks
     results = data.frame(NumberOfPack = numeric(), Value = numeric(), Price = numeric(),
                          TypeOfDeck = character(), NormalCards = numeric(), 
@@ -10,12 +15,18 @@ sim = function(n, balance){
   # Iterating through the number of openings 
     for (idx in (1:n)){
       
-    TypeOfDeck = sample(c('normal','uncommon','special'), 1)
+    TypeOfDeck = sample(c('normal','uncommon','special'), 1, prob = Prob_deck)
     price = check_price(TypeOfDeck)
-    deck_list = buy_deck(TypeOfDeck, balance)
-    deck = deck_list$deck
-    balance = deck_list$balance
+    deck_list = buy_deck(TypeOfDeck, Balance,
+                         Val_norm = Val_norm, Prob_norm = Prob_norm,
+                         Val_unc = Val_unc, Prob_unc = Prob_unc,
+                         Val_spec = Val_spec, Prob_spec = Prob_spec)
     
+    deck = deck_list$deck
+    Balance = deck_list$balance
+    
+    # If deck is indeed NULL, that means that we've got no money, hence the loop is broken
+    # and we remain with the already opened packs
     if (is.null(deck)){
       message('You went broke, simulation is stopped!')
       break}
@@ -30,12 +41,17 @@ sim = function(n, balance){
                      Profit = sum(deck$Value) - price)
     results = rbind(results, res)
     }
-    
+  
+  # We plot out how much have we won or lost 
   print(flexplot(Profit ~ NumberOfPack, data = results, method = 'lm'))
+  
+  end = Sys.time()
+  print(end-start)
+  
   return(results)
 }
 
-res = sim(500, 100000)
-res
+res = sim(n = 100, Balance = 30000, Prob_deck = c(.1,.45,.45))
+table(res$TypeOfDeck)
 
 
